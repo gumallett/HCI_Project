c7eee835e7fc66e768004c3bd49ce6968d6f216b
Greg Mallett <gregory.mallett@nytimes.com>
2014-10-26 14:51:15 -0400

    styling

--- src/views/home.php
+++ src/views/home.php
@@ -1,12 +1,13 @@
+<div id="homeHeader" class="center-block">
+   <h1>WELCOME <small>Deep Learning</small></h1>
+</div>
 
 <div class="center-block" id="searchForm">
-   <h1>WELCOME <small>Deep Learning</small></h1>
-
    <form role="form" action="/searchResults" method="get">
       <div class="form-group">
          <label for="searchTerms">Search:</label>
          <input class="form-control" type="text" id="searchTerms" name="searchTerms" placeholder="Search">
       </div>
-      <button id="searchSubmit" type="submit" class="btn btn-default"><strong>Go!</strong></button>
+      <button id="searchSubmit" type="submit" class="btn btn-success"><strong>Go!</strong></button>
    </form>
 </div>
