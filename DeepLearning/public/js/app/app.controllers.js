266b2149052e32917afd22d89ce3574bbf8e88b8
Greg Mallett <gregory.mallett@nytimes.com>
2014-12-07 20:54:26 -0500

    quiz validation

--- DeepLearning/public/js/app/app.controllers.js
+++ DeepLearning/public/js/app/app.controllers.js
@@ -16,12 +16,45 @@ app.controller('TopicCtrl', function($scope, $rootScope, $state, $stateParams, T
     $scope.topicName = $stateParams['topic'];
 });
 
-app.controller('QuizCtrl', function($scope, $state, $stateParams, TopicResource, foundQuiz) {
+app.controller('QuizCtrl', function($scope, $state, $stateParams, $log, TopicResource, foundQuiz) {
     $scope.quiz = foundQuiz;
+    $scope.submittedAnswers = [];
+    $log.log($scope.quiz);
+    $scope.correctAnswers = function() {
+        var correctAnswerList = [];
+
+        foundQuiz.questions.forEach(function(question, idx) {
+
+            for(var i = 0; i < question.answers.length; i++) {
+                if(question.answers[i].correct) {
+                    correctAnswerList[idx] = i;
+                }
+                else if(angular.isUndefined(correctAnswerList[idx]) && i == question.answers.length-1) {
+                    correctAnswerList[idx] = 'none';
+                }
+            }
+        });
+
+        return correctAnswerList;
+    };
     $scope.quizEditing = $state.is('topicQuiz.topicQuizEditing');
 
     $scope.edit = function() {
         $scope.quizEditing = true;
         $state.go('.topicQuizEditing', {topic: $stateParams['topic'], quiz: $stateParams['quiz']});
-    }
+    };
+
+    $scope.submit = function() {
+        $scope.submitted = true;
+        var form = $scope.quizForm;
+        $log.log(form);
+        $log.log(form.answer);
+        $log.log($scope.submittedAnswers);
+        $log.log($scope.correctAnswers());
+    };
+
+    $scope.reset = function() {
+        $scope.submitted = false;
+        $scope.submittedAnswers = [];
+    };
 });
